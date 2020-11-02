fc3b4ee58680f7216050d30ed598b6cd262d6773
Justin Hopper <doonze@gmail.com>
2020-11-02 00:39:22 -0600

    added try/catch to most functions and fixed logic for errors during race editing

--- modules/functions.py
+++ modules/functions.py
@@ -7,12 +7,15 @@ from modules.custom_classes import Colors, Race
 import json
 import os
 from modules.options import user_options
+from typing import get_type_hints
 
 cb = Colors.BROWN
 ce = Colors.END
 cr = Colors.RED
 cbl = Colors.BLINK
 cg = Colors.GREEN
+
+
 # todo: need to give all classes doc
 
 
@@ -106,7 +109,7 @@ def stat_bonus(stat: int):
 
 
 def feet_inch(inches: int):
-    """Pass function a numner of inches and it will return Feet' inches" ex: 5'6"
+    """Pass function a number of inches and it will return Feet' inches" ex: 5'6"
     """
     converted = divmod(inches, 12)
     feet = converted[0]
@@ -119,108 +122,120 @@ def save_char(save_dict: dict, file_name: str):
     """Pass function a character dictionary and a file name. It will save it to the specified file.\n
      If the file exist it will append to it, if not it will create it.
      """
-    save_dict = [save_dict]
-    keyed_dict = dict((item['name'], item) for item in save_dict)
+    try:
+        save_dict = [save_dict]
+        keyed_dict = dict((item['name'], item) for item in save_dict)
 
-    if os.path.exists(f'saves/{file_name}.json'):
-        with open(f'saves/{file_name}.json') as f:
-            loaded_json = json.load(f)
-        loaded_json.update(keyed_dict)
-        json_save = json.dumps(loaded_json, indent=2)
-    else:
-        json_save = json.dumps(keyed_dict, indent=2)
+        if os.path.exists(f'saves/{file_name}.json'):
+            with open(f'saves/{file_name}.json') as f:
+                loaded_json = json.load(f)
+            loaded_json.update(keyed_dict)
+            json_save = json.dumps(loaded_json, indent=2)
+        else:
+            json_save = json.dumps(keyed_dict, indent=2)
 
-    f = open(f"saves/{file_name}.json", 'w')
-    f.write(json_save)
-    f.close()
-
-
-def pull_saved_char(file_name='char', pull_type="name"):
-    with open(f'saves/{file_name}.json') as f:
-        loaded_json = json.load(f)
-
-    if pull_type == 'name':
-        name_list = []
-        for name in loaded_json:
-            name_list.append(name)
-        return sorted(name_list, key=str.lower)
+        f = open(f"saves/{file_name}.json", 'w')
+        f.write(json_save)
+        f.close()
+    except Exception as ex:
+        print(f'Something went wrong converting list to numbered dictionary: {ex}')
 
 
 def list_to_num_dict(char_list):
-    name_dict = {}
-    num = 1
+    try:
+        name_dict = {}
+        num = 1
 
-    for name in char_list:
-        name_dict[f'{num}'] = name
-        num += 1
+        for name in char_list:
+            name_dict[f'{num}'] = name
+            num += 1
 
-    return name_dict
+        return name_dict
+    except Exception as ex:
+        print(f'Something went wrong converting list to numbered dictionary: {ex}')
 
 
 def print_list(input_var, var_type='list', begin='', end='', new_line=True):
-    if var_type == "list":
-        for each in input_var:
-            if new_line:
-                typed_print(f'{begin}{each}{end}')
-            else:
-                typed_print(f'{begin}{each}{end}', new_line=False)
-    elif var_type == 'num_list':
-        num = 1
-        for each in input_var:
-            if new_line:
-                typed_print(f'{Colors.BROWN}({num}){Colors.END} {each}')
-                num += 1
-            else:
-                typed_print(f'{Colors.BROWN}({num}){Colors.END} {each}', new_line=False)
-                num += 1
-    elif var_type == 'dict':
-        for i in input_var:
-            typed_print(f'{Colors.BROWN}({i}){Colors.END} {input_var[i]}')
+    try:
+        if var_type == "list":
+            for each in input_var:
+                if new_line:
+                    typed_print(f'{begin}{each}{end}')
+                else:
+                    typed_print(f'{begin}{each}{end}', new_line=False)
+        elif var_type == 'num_list':
+            num = 1
+            for each in input_var:
+                if new_line:
+                    typed_print(f'{Colors.BROWN}({num}){Colors.END} {each}')
+                    num += 1
+                else:
+                    typed_print(f'{Colors.BROWN}({num}){Colors.END} {each}', new_line=False)
+                    num += 1
+        elif var_type == 'dict':
+            for i in input_var:
+                typed_print(f'{Colors.BROWN}({i}){Colors.END} {input_var[i]}')
+    except Exception as ex:
+        print(f'Something went wrong printing from list in print_list: {ex}')
 
 
-def pull_saved_data_names(path_file_name):
-    with open(f'{path_file_name}') as f:
-        loaded_json = json.load(f)
-        name_list = []
-        for race in loaded_json:
-            name_list.append(race)
-        return sorted(name_list, key=str.lower)
+def pull_saved_data_indexes(path_file_name):
+    try:
+        with open(f'{path_file_name}') as f:
+            loaded_json = json.load(f)
+            index_list = []
+            for index in loaded_json:
+                index_list.append(index)
+            return sorted(index_list, key=str.lower)
+    except Exception as ex:
+        print(f'Something went wrong pulling saved data indexes from dictionary: {ex}')
 
 
 def pull_saved_data(path_file_name: str, index_name: str, class_name):
-    with open(f'{path_file_name}') as f:
-        loaded_json = json.load(f)
-        index_dict = loaded_json[index_name]
-        filled_class = class_name(**index_dict)
-        return filled_class
+    try:
+        with open(f'{path_file_name}') as f:
+            loaded_json = json.load(f)
+            index_dict = loaded_json[index_name]
+            filled_class = class_name(**index_dict)
+            return filled_class
+    except Exception as ex:
+        print(f'Something went wrong pulling saved data: {ex}')
 
 
 def print_class_data(data_class):
     try:
-        field_list = []
+        field_dict = {}
         for field in fields(data_class):
-            field_list.append(field.name)
+            field_dict[field.name] = field.type
             print(f'{field.name.capitalize():<8}: {cb}{str(getattr(data_class, field.name)):>2}{ce}')
-        return field_list
+        return field_dict
     except Exception as ex:
-        print(f'Something went wrong: {ex}')
+        print(f'Something went wrong printing class data: {ex}')
 
 
-def edit_class_data(pulled_race, menu_choice: str):
+def edit_class_data(pulled_race, menu_choice: str, field_dict: dict):
     try:
-        typed_print(f'Editing value {cb}[{menu_choice.capitalize()}]{ce}. Enter list with the []\'s. '
+        typed_print(f'Editing value {cb}[{menu_choice.capitalize()}]{ce}. Enter list separated by ,. '
                     f'The current value is {cb}[{getattr(pulled_race, menu_choice)}]{ce}: {cb}',
                     new_line=False)
+        set_type = field_dict[menu_choice]
         while True:
             response = input()
             print(ce, end='')
-            setattr(pulled_race, menu_choice, response)
+            if set_type is list:
+                response = response.translate(str.maketrans('', '', ' []'))
+                response = [int(i) for i in response.split(',')]
+                setattr(pulled_race, menu_choice, set_type(response))
+            else:
+                setattr(pulled_race, menu_choice, set_type(response))
             clear_screen()
             print_class_data(pulled_race)
-            return pulled_race
-            break
+            success = True
+            return pulled_race, success
     except Exception as ex:
         print(f'Something went wrong: {ex}')
+        success = False
+        return pulled_race, success
 
 
 def save_dictionary(save_dict: dict, path_file_name: str, index: str):
@@ -234,17 +249,20 @@ def save_dictionary(save_dict: dict, path_file_name: str, index: str):
      data/options.json\n
      saves/char.json\n
      """
-    save_dict = [save_dict]
-    keyed_dict = dict((item[index], item) for item in save_dict)
+    try:
+        save_dict = [save_dict]
+        keyed_dict = dict((item[index], item) for item in save_dict)
 
-    if os.path.exists(f'{path_file_name}'):
-        with open(f'{path_file_name}') as f:
-            loaded_json = json.load(f)
-        loaded_json.update(keyed_dict)
-        json_save = json.dumps(loaded_json, indent=4)
-    else:
-        json_save = json.dumps(keyed_dict, indent=4)
+        if os.path.exists(f'{path_file_name}'):
+            with open(f'{path_file_name}') as f:
+                loaded_json = json.load(f)
+            loaded_json.update(keyed_dict)
+            json_save = json.dumps(loaded_json, indent=4)
+        else:
+            json_save = json.dumps(keyed_dict, indent=4)
 
-    f = open(f"{path_file_name}", 'w')
-    f.write(json_save)
-    f.close()
+        f = open(f"{path_file_name}", 'w')
+        f.write(json_save)
+        f.close()
+    except Exception as ex:
+        print(f'Something went wrong in the save_dictionary function: {ex}')
