4a1769018ff7df56cff18980b5c8d455d26b6d40
Justin Hopper <doonze@gmail.com>
2020-11-02 00:42:01 -0600

    using universal pull for saved game pull, saved index, changed fields pull from list to dict w/types

--- modules/menu.py
+++ modules/menu.py
@@ -1,6 +1,7 @@
 from modules.create_char import char_creation
 from modules.opening_text import *
 
+
 # todo: build options menu, with pull, edit, save
 # This is the initial start menu
 def start_menu():
@@ -45,7 +46,7 @@ def saved_game_menu():
     clear_screen()
     typed_print('Here are the current saved games:')
     print()
-    print_list(list_to_num_dict(pull_saved_char()), var_type='dict')
+    print_list(list_to_num_dict(pull_saved_data_indexes('saves/char.json')), var_type='dict')
     print()
     typed_print(f'Select a saved game above or (C) to cancel {cb}[?, c]{ce}:{cb} ', new_line=False)
 
@@ -105,10 +106,11 @@ def admin_menu():
 
     start_menu()
 
+
 # todo: Need to add ways to create NEW races, and delete races
 def races_admin_menu():
     clear_screen()
-    pulled_saved_items = pull_saved_data_names('data/races.json')
+    pulled_saved_items = pull_saved_data_indexes('data/races.json')
     item_dict = list_to_num_dict(pulled_saved_items)
     typed_print('This is the administration menu for Races.')
     print()
@@ -136,23 +138,30 @@ def races_admin_edit(race):
     typed_print(f'You chose to edit {race}, here are the current values:')
     print()
     pulled_race = pull_saved_data('data/races.json', race, Race)
-    field_list = print_class_data(pulled_race)
+    field_dict = print_class_data(pulled_race)
     print()
-    typed_print(f'Enter a field to edit, or (C) to return to Races menu. '
+    typed_print(f'Enter a field to edit, (N) to create new race, or (C) to return to Races menu. '
                 f'Example {cb}[str]{ce}:{cb} ', new_line=False)
 
     while True:
         menu_choice = input().lower()
         print(ce, end='')
-        if menu_choice in field_list:
-            edited_race = edit_class_data(pulled_race, menu_choice)
-            print()
-            typed_print(f"Value was updated, enter another to edit or (S) to save: {cb}", new_line=False)
+        if menu_choice in field_dict:
+            edited_race = edit_class_data(pulled_race, menu_choice, field_dict)
+            if edited_race[1] is True:
+                print()
+                typed_print(f"Value was updated, enter another to edit or (S) to save: {cb}", new_line=False)
+            else:
+                typed_print(f'There was an error. Enter a field to edit, or (C) to return to Races menu. '
+                            f'Example {cb}[str]{ce}:{cb} ', new_line=False)
             continue
         elif menu_choice == 'c':
+            races_admin_menu()
+            break
+        elif menu_choice == 'n':
             break
         elif menu_choice == 's':
-            save_dictionary(edited_race.__dict__, 'data/races.json', 'race')
+            save_dictionary(edited_race[0].__dict__, 'data/races.json', 'race')
             break
         else:
             typed_print(f'Value entered: {cb}{menu_choice}{ce} is not valid, please reenter: {cb} ', new_line=False)
