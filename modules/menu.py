d1fe25f5d8aa5a53869e976dde8a3f8e739aba9d
Justin Hopper <doonze@gmail.com>
2020-11-08 22:51:06 -0600

    added save games menu and other menu enhancments

--- modules/menu.py
+++ modules/menu.py
@@ -1,9 +1,8 @@
 from modules.create_char import char_creation
+from modules.custom_classes import Race, Archetype, Player
 from modules.opening_text import *
-from modules.custom_classes import Race, UserOptions
 
 
-# todo: build options menu, with pull, edit, save
 # This is the initial start menu
 def start_menu():
     """Main menu"""
@@ -23,24 +22,24 @@ def start_menu():
     typed_print(f"Please enter an option {cb}[1-5,e]{ce}:{cb} ", new_line=False)
 
     while True:
-        load_saved = input()
+        response = input()
         print(f'{ce}', end='')
-        if load_saved == '1':
+        if response == '1':
             opening_text()
             break
-        elif load_saved == '2':
+        elif response == '2':
             saved_game_menu()
             break
-        elif load_saved == '3':
+        elif response == '3':
             char_creation()
             break
-        elif load_saved == '4':
+        elif response == '4':
             admin_menu()
             break
-        elif load_saved == '5':
+        elif response == '5':
             options_menu()
             break
-        elif load_saved.lower() == 'e':
+        elif response.lower() == 'e':
             raise SystemExit
         else:
             typed_print(f'Invalid option! Enter a number 1-5 or e to exit! {cb}[1-5,e]{ce}: ', new_line=False)
@@ -49,31 +48,55 @@ def start_menu():
 def saved_game_menu():
     """List all saved games"""
     clear_screen()
+    pulled_saved_items = pull_saved_data_indexes('saves/char.json')
+    item_dict = list_to_num_dict(pulled_saved_items)
     typed_print('Here are the current saved games:')
     print()
-    print_list(list_to_num_dict(pull_saved_data_indexes('saves/char.json')), var_type='dict')
+    print_list(item_dict, var_type='dict')
     print()
-    typed_print(f'Select a saved game above or (C) to cancel {cb}[?, c]{ce}: ', new_line=False)
+    typed_print(f'Select a saved game above or (C) to cancel {cb}[?, c]{ce}:{cb} ', new_line=False)
 
     while True:
-        load_saved = input()
+        menu_choice = input()
         print(ce, end='')
-        if load_saved == '1':
-
+        if menu_choice in item_dict.keys():
+            load_saved(item_dict[menu_choice])
             break
-        elif load_saved == '3':
-
-            break
-        elif load_saved == '4':
-
-            break
-        elif load_saved.lower() == 'c':
+        elif menu_choice == 'c':
             break
         else:
-            typed_print(f'Invalid option! Select a saved game above or (C) to cancel {cb}[1-3,e]{ce}:{cb} '
-                        f'', new_line=False)
+            typed_print(f'Invalid option! Enter a number or c to return to admin menu! '
+                        f'{cb}[?,c]{ce}:{cb} ', new_line=False)
 
     start_menu()
+
+
+def load_saved(saved_game: str):
+    clear_screen()
+    typed_print(f'You chose to edit {saved_game}, here are the current values:')
+    print()
+
+    char_build: Player
+    char_build = pull_saved_data('saves/char.json', saved_game)
+
+    typed_print(f"You are a {char_build.Player_race.Race_name} {char_build.Player_type.Name}"
+                f" named {char_build.Player_name}")
+    typed_print(f"Height: {char_build.Height}")
+    typed_print(f"Weight: {char_build.Weight} lbs")
+    typed_print(f"Age: {char_build.Age}")
+    typed_print(f'Hit point: {char_build.HP}')
+    typed_print(f'Armor Class: {char_build.AC}')
+    print()
+    typed_print(f"{'Attribute':<14} {'Stat':<4} Mod")
+    typed_print('-----------------------')
+    typed_print(f"{'Strength:':<14} {char_build.Str:<4} {stat_bonus(char_build.Str, colored=True)}")
+    typed_print(f"{'Dexterity:':<14} {char_build.Dex:<4} {stat_bonus(char_build.Dex, colored=True)}")
+    typed_print(f"{'Constitution:':<14} {char_build.Con:<4} {stat_bonus(char_build.Con, colored=True)}")
+    typed_print(f"{'Wisdom:':<14} {char_build.Wis:<4} {stat_bonus(char_build.Wis, colored=True)}")
+    typed_print(f"{'Intelligence:':<14} {char_build.Int:<4} {stat_bonus(char_build.Int, colored=True)}")
+    typed_print(f"{'Charisma:':<14} {char_build.Cha:<4} {stat_bonus(char_build.Cha, colored=True)}")
+    print()
+    input()
 
 
 def admin_menu():
@@ -84,7 +107,7 @@ def admin_menu():
     print()
     admin_items = {
         1: 'Races admin',
-        2: 'Class admin',
+        2: 'Archetype admin',
         3: '...',
         4: '...',
         'c': 'Return to main menu!'
@@ -99,8 +122,8 @@ def admin_menu():
         if menu_choice == '1':
             races_admin_menu()
             break
-        elif menu_choice == '3':
-
+        elif menu_choice == '2':
+            archetype_admin_menu()
             break
         elif menu_choice == '4':
 
@@ -113,7 +136,6 @@ def admin_menu():
     start_menu()
 
 
-# todo: Need to add ways to create NEW races, and delete races
 def races_admin_menu():
     """Menu for editing, creating, or deleting races"""
     clear_screen()
@@ -123,7 +145,7 @@ def races_admin_menu():
     print()
     print_list(item_dict, var_type='dict')
     print()
-    typed_print(f'Choose a choice above, (N) to create new race,  or (C) to return to the admin menu'
+    typed_print(f'Choose a Race above, ({cb}N{ce}) to create new race,  or ({cb}C{ce}) to return to the admin menu'
                 f' {cb}[?, c]{ce}:{cb} ', new_line=False)
 
     while True:
@@ -133,7 +155,6 @@ def races_admin_menu():
             races_admin_edit(item_dict[menu_choice])
             break
         elif menu_choice == 'c':
-            admin_menu()
             break
         elif menu_choice == 'n':
 
@@ -152,20 +173,18 @@ def races_admin_edit(race, new=False):
     if new:
         typed_print(f"You've chosen to create a new Race.")
     else:
-        typed_print(f'You chose to edit {race}, here are the current values:')
+        typed_print(f'You chose to edit {cb}{race}{ce}, here are the current values:')
     print()
-
     if not new:
-        pulled_race = pull_saved_data('data/races.json', race, Race)
+        pulled_race = pull_saved_data('data/races.json', race)
     else:
         pulled_race = race
     field_dict = print_class_data(pulled_race)
     print()
-    typed_print(f'Enter a field to edit, (D) to delete race, or (C) to return to Races menu. '
+    typed_print(f'Enter a field to edit, ({cb}D{ce}) to delete race, or ({cb}C{ce}) to return to Races menu. '
                 f'Example {cb}[Str]{ce}:{cb} ', new_line=False)
 
     while True:
-        edited_race = ()
         menu_choice = input()
         print(ce, end='')
         if menu_choice.lower().capitalize() in field_dict:
@@ -173,16 +192,16 @@ def races_admin_edit(race, new=False):
             edited_race = edit_class_data(pulled_race, menu_choice, field_dict)
             if edited_race[1] is True:
                 print()
-                typed_print(f"Value was updated, enter another to edit or (S) to save: {cb}", new_line=False)
+                typed_print(f"{cy}SUCCESS!{ce}, enter another to edit, ({cb}C{ce}) to cancel,"
+                            f" or ({cb}S{ce}) to save: {cb}", new_line=False)
             else:
-                typed_print(f'There was an error. Enter a field to edit, or (C) to return to Races menu. '
+                typed_print(f'There was an error. Enter a field to edit, or ({cb}C{ce}) to return to Races menu. '
                             f'Example {cb}[Str]{ce}:{cb} ', new_line=False)
             continue
         elif menu_choice.lower() == 'c':
             break
         elif menu_choice.lower() == 's':
-            save_dictionary(edited_race[0].__dict__, 'data/races.json', 'Race_name'
-                                                                        '')
+            save_dictionary(jsonpickle.encode(edited_race[0]), 'data/races.json', edited_race[0].Race_name)
             break
         elif menu_choice.lower() == 'd':
             result = input(f'Are you SURE you wish to {cr}DELETE{ce} Race: {cb}{race}{ce} [yes,n]? ')
@@ -230,7 +249,7 @@ def options_edit(options):
     typed_print(f'You chose to edit {options}, here are the current values:')
     print()
 
-    pulled_options = pull_saved_data('data/options.json', options, UserOptions)
+    pulled_options = pull_saved_data('data/options.json', options)
 
     field_dict = print_class_data(pulled_options, "<15", '')
 
@@ -255,7 +274,7 @@ def options_edit(options):
         elif menu_choice.lower() == 'c':
             break
         elif menu_choice.lower() == 's':
-            save_dictionary(edited_options[0].__dict__, 'data/options.json', 'Type')
+            save_dictionary(jsonpickle.encode(edited_options[0]), 'data/options.json', edited_options[0].Type)
             break
         else:
             typed_print(f'Value entered: {cb}{menu_choice}{ce} is not valid, please reenter: {cb} ', new_line=False)
@@ -263,3 +282,83 @@ def options_edit(options):
     # If 'c' is chosen the loop it broken and we return to prev menu, this is used so we don't get unending while
     # loops nestled going through the menu's
     options_menu()
+
+
+def archetype_admin_menu():
+    """Menu for editing, creating, or deleting archetypes"""
+    clear_screen()
+    pulled_saved_items = pull_saved_data_indexes('data/archetype.json')
+    item_dict = list_to_num_dict(pulled_saved_items)
+    typed_print('This is the administration menu for Archetypes.')
+    print()
+    print_list(item_dict, var_type='dict')
+    print()
+    typed_print(f'Choose a choice above, (N) to create new Archetype,  or (C) to return to the admin menu'
+                f' {cb}[?, c]{ce}:{cb} ', new_line=False)
+
+    while True:
+        menu_choice = input().lower()
+        print(ce, end='')
+        if menu_choice in item_dict.keys():
+            archetype_admin_edit(item_dict[menu_choice])
+            break
+        elif menu_choice == 'c':
+            break
+        elif menu_choice == 'n':
+            archetype_admin_edit(Archetype(), new=True)
+            break
+        else:
+            typed_print(f'Invalid option! Enter a number or c to return to admin menu! '
+                        f'{cb}[?,c]{ce}:{cb} ', new_line=False)
+
+    admin_menu()
+
+
+def archetype_admin_edit(archetype, new=False):
+    """The actual edit interface for archetype"""
+    clear_screen()
+    if new:
+        typed_print(f"You've chosen to create a new Archetype.")
+    else:
+        typed_print(f'You chose to edit {archetype}, here are the current values:')
+    print()
+
+    if not new:
+        pulled_archetype = pull_saved_data('data/archetype.json', archetype)
+    else:
+        pulled_archetype = archetype
+    field_dict = print_class_data(pulled_archetype)
+    print()
+    typed_print(f'Enter a field to edit, (D) to delete race, or (C) to return to Races menu. '
+                f'Example {cb}[Str]{ce}:{cb} ', new_line=False)
+
+    while True:
+        menu_choice = input()
+        print(ce, end='')
+        if menu_choice.lower().capitalize() in field_dict:
+            menu_choice = menu_choice.lower().capitalize()
+            edited_archetype = edit_class_data(pulled_archetype, menu_choice, field_dict)
+            if edited_archetype[1] is True:
+                print()
+                typed_print(f"Value was updated, enter another to edit or (S) to save: {cb}", new_line=False)
+            else:
+                typed_print(f'There was an error. Enter a field to edit, or (C) to return to Races menu. '
+                            f'Example {cb}[Str]{ce}:{cb} ', new_line=False)
+            continue
+        elif menu_choice.lower() == 'c':
+            break
+        elif menu_choice.lower() == 's':
+            save_dictionary(jsonpickle.encode(edited_archetype[0]), 'data/archetype.json', edited_archetype[0].Name)
+            break
+        elif menu_choice.lower() == 'd':
+            result = input(f'Are you SURE you wish to {cr}DELETE{ce} Race: {cb}{archetype}{ce} [yes,n]? ')
+            if result.lower() == 'yes':
+                edited_archetype = {}
+                save_dictionary(edited_archetype, 'data/archetype.json', archetype, del_dict=True)
+            break
+        else:
+            typed_print(f'Value entered: {cb}{menu_choice}{ce} is not valid, please reenter: {cb} ', new_line=False)
+
+    # If 'c' is chosen the loop it broken and we return to prev menu, this is used so we don't get unending while
+    # loops nestled going through the menu's
+    archetype_admin_menu()
