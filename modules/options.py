24927e08dede72eac13a82b69432af5766cdc34f
Justin Hopper <doonze@gmail.com>
2020-11-08 22:51:42 -0600

    using jsonpickle now

--- modules/options.py
+++ modules/options.py
@@ -1,10 +1,11 @@
 # The file is just for creating global options
 # from modules.functions import pull_saved_data
+import jsonpickle
 from modules.custom_classes import UserOptions
 import json
 
 
-def pull_saved_data(path_file_name: str, index_name: str, class_name):
+def pull_saved_data(path_file_name: str, index_name: str) -> UserOptions:
     """
     One off function just to pull game options.
 
@@ -12,17 +13,16 @@ def pull_saved_data(path_file_name: str, index_name: str, class_name):
     :type path_file_name: str
     :param index_name: Indexed name to pull
     :type index_name: str
-    :param class_name: Name of class to build
-    :type class_name:
-    :return: None
-    :rtype: None
+    :return: UserOptions
+    :rtype: any
     """
     with open(f'{path_file_name}') as f:
         loaded_json = json.load(f)
         index_dict = loaded_json[index_name]
-        filled_class = class_name(**index_dict)
+        filled_class = jsonpickle.decode(index_dict)
         return filled_class
 
 
-user_options = UserOptions
-user_options = pull_saved_data('data/options.json', 'User Options', UserOptions)
+user_options: UserOptions
+user_options = pull_saved_data('data/options.json', 'User Options')
+
